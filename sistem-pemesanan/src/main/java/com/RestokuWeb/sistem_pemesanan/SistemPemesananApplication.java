486be2c5348364378685c82c3c4039c61cb82be4
MuhammadFauzanL <fauzanlubada5@gmail.com>
2025-06-25 10:02:18 +0700

    firrst

---
+++ sistem-pemesanan/src/main/java/com/RestokuWeb/sistem_pemesanan/SistemPemesananApplication.java
@@ -0,0 +1,13 @@
+package com.RestokuWeb.sistem_pemesanan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemPemesananApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemPemesananApplication.class, args);
+    }
+
+}
---
+++ sistem-pemesanan/src/test/java/com/RestokuWeb/sistem_pemesanan/SistemPemesananApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RestokuWeb.sistem_pemesanan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemPemesananApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
